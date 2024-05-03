cdf7ae9602d3d7d6990874a6804dc7c9ddd38ef4
AliEmadEldin <aly.emad79@gmail.com>
2024-05-03 14:25:10 +0300

    ..

--- bus_reservation_sys/src/bus_reservation_sys/Bus_reservation_sys.java
+++ bus_reservation_sys/src/bus_reservation_sys/Bus_reservation_sys.java
@@ -1,7 +1,7 @@
 package bus_reservation_sys;
 public class Bus_reservation_sys {
     public static void main(String[] args) {
-        Welcome W = new Welcome();
-        W.show();
+        Welcome W = new Welcome(); //first page
+        W.show();   // first page appearance
     }    
 }
